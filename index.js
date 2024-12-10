e6944911791088da57490f42dc231de3d0503446
Jason Hall <jason.hall.sde@gmail.com>
2024-12-10 04:59:42 -0800

    second commit (will say changes later)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const video = document.getElementById("header__video");
 const muteButton = document.getElementById("mute-button");
+const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
 function openMenu() {
   document.body.classList += " menu--open";
@@ -25,6 +26,11 @@ function toggleMute() {
   } else {
     muteButton.innerHTML = '<i class="fa-solid fa-volume-high"></i>'; // Unmuted icon
   }
+}
+
+if (isMobile) {
+  const video = document.getElementById("myVideo");
+  video.controls = false; // Disable controls on mobile
 }
 
 muteButton.addEventListener("click", toggleMute);
